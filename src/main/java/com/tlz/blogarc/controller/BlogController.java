f8f812f087efe5a74f412b020859269d67e9a536
TheLordZedd <pcgpod@yahoo.com>
2023-10-24 15:50:38 -0500

    - Added search feature for client side

--- src/main/java/com/tlz/blogarc/controller/BlogController.java
+++ src/main/java/com/tlz/blogarc/controller/BlogController.java
@@ -5,6 +5,8 @@ import com.tlz.blogarc.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,4 +26,26 @@ public class BlogController {
         model.addAttribute("postsResponse", postResponse);
         return "blog/view_posts";
     }
+
+    // handles view post request
+    @GetMapping("/post/{postUrl}")
+    public String showPost(@PathVariable("postUrl") String postUrl,
+                           Model model) {
+        PostDTO post = postService.findPostByUrl(postUrl);
+        model.addAttribute("post", post);
+        return "blog/blog_post";
+
+    }
+
+    // handles blog post search request
+    @GetMapping("/page/search")
+    public String searchPosts(@RequestParam(value = "query") String query,
+                              Model model) {
+        List<PostDTO> postsResponse = postService.searchPosts(query);
+        model.addAttribute("postsResponse", postsResponse);
+        return "blog/view_posts";
+    }
+
+
+
 }
